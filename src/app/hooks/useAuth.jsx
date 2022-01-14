12157b694d4aeedf71428748523da920d8ba87de
AlisaWonder <korenskayaalice@gmail.com>
2022-01-14 18:27:51 +0300

    signUp method finished and redirect done

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import { toast } from "react-toastify";
 import userService from "../services/user.service";
+import { setTokens } from "../services/localStorage.service";
 
 const httpAuth = axios.create();
 const AuthContext = React.createContext();
@@ -10,19 +11,10 @@ const AuthContext = React.createContext();
 export const useAuth = () => {
     return useContext(AuthContext);
 };
-const TOKEN_KEY = "jwt-token";
-const REFRESH_KEY = "jwt-referesh-token";
-const EXPIRES_KEY = "jwt-expires";
 
 const AuthProvider = ({ children }) => {
     const [currentUser, setUser] = useState({});
     const [error, setError] = useState(null);
-    function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
-        const expiresDate = new Date().getTime() + expiresIn * 1000;
-        localStorage.setItem(TOKEN_KEY, idToken);
-        localStorage.setItem(REFRESH_KEY, refreshToken);
-        localStorage.setItem(EXPIRES_KEY, expiresDate);
-    }
 
     async function signUp({ email, password, ...rest }) {
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
@@ -37,6 +29,17 @@ const AuthProvider = ({ children }) => {
             await createUser({ _id: data.localId, email, ...rest });
         } catch (error) {
             errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            console.log(code, message);
+            if (code === 400) {
+                if (message === "EMAIL_EXISTS") {
+                    const errorObject = {
+                        email: "Пользователь с таким email уже существует"
+                    };
+                    throw errorObject;
+                }
+                // throw new Error
+            }
         }
     }
 
